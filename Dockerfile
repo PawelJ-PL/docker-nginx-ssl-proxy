80474d43ab5616f6071043a5c78d9b86fa0e044c
Pawel <inne.poczta@gmail.com>
2019-04-26 23:35:48 +0200

    Generate certificate on container startup

--- Dockerfile
+++ Dockerfile
@@ -3,13 +3,14 @@ ARG BASE_IMAGE_TAG
 FROM nginx:${BASE_IMAGE_TAG}
 
 RUN apt-get update; \
-    apt-get install -y iproute2
+    apt-get install -y iproute2 openssl
 
 ENV LISTEN_PORT=8443
 ENV DST_PORT=80
+ENV CERT_CN=127.0.0.1
 
-COPY cert.pem /tmp/cert.pem
-COPY key.pem /tmp/key.pem
+COPY rootCA.crt /tmp/rootCA.crt
+COPY rootCA.key /tmp/rootCA.key
 COPY proxy_ssl.conf.template /tmp/proxy_ssl.conf.template
 COPY run_command.sh /run_command.sh
 
--- run_command.sh
+++ run_command.sh
@@ -2,6 +2,21 @@
 
 export HOST_ADDR=`/sbin/ip route|awk '/default/ { print $3 }'`
 
+openssl genrsa -out /tmp/server.key 4096
+
+openssl req -new -sha256 -key /tmp/server.key -subj "/C=PL/CN=${CERT_CN}" -out /tmp/server.csr
+echo "Generated CSR:"
+openssl req -in /tmp/server.csr -noout -text
+echo
+
+echo
+openssl x509 -req -in /tmp/server.csr -CA /tmp/rootCA.crt -CAkey /tmp/rootCA.key -CAcreateserial -out /tmp/server.crt -days 365 -sha256 -passin pass:secret
+echo "Generated Certificate:"
+openssl x509 -in /tmp/server.crt -text -noout
+echo
+
+cat /tmp/server.crt /tmp/rootCA.crt > /tmp/bundle.crt
+
 envsubst < /tmp/proxy_ssl.conf.template > /etc/nginx/conf.d/proxy_ssl.conf
 exec nginx -p /tmp -g 'daemon off;'
 
